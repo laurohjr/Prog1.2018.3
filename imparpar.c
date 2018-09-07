b5289b5b57e722620f233107c3da1a994b7b63cb
laurohjr <43019037+laurohjr@users.noreply.github.com>
2018-09-06 21:39:22 -0300

    Add files via upload

---
+++ imparpar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, resto;
+    printf("entre com n1\n");
+    scanf("%d",&n1);
+    resto=n1%2;
+    if(resto==0){
+              printf("\n o numero e par\n");
+              
+    }                    
+    else {
+              printf("\n o numero impar\n");
+         } 
+              
+              system("pause");
+        
+return 0;
+}                            
